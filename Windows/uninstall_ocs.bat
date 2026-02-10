52b6ed90039994a861523dc9953568952db8ec31
Diego7740 <d.morenozuniga24@gmail.com>
2026-02-10 12:38:00 -0300

    Create uninstall_ocs.bat

---
+++ Windows/uninstall_ocs.bat
@@ -0,0 +1,14 @@
+@echo off
+
+sc stop "OCS Inventory Service" >nul 2>&1
+timeout /t 5 >nul
+
+IF EXIST "C:\Program Files\OCS Inventory Agent\uninst.exe" ("C:\Program Files\OCS Inventory Agent\uninst.exe" /S)
+
+rmdir /s /q "C:\Program FilesOCS Inventory Agent" 2>nul
+
+sc delete "OCS Inventory Service" >nul 2>&1
+
+schtask /delete /tn "OCS Inventory Agent" /f >nul 2>&1
+
+exit /b 0
