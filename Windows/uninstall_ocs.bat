760a71f3dd17e81ceb05072943e8a85e1cb83328
Diego7740 <d.morenozuniga24@gmail.com>
2026-02-13 09:40:09 -0300

    Fix path in uninstall script for OCS Inventory Agent

--- Windows/uninstall_ocs.bat
+++ Windows/uninstall_ocs.bat
@@ -5,7 +5,7 @@ timeout /t 5 >nul
 
 IF EXIST "C:\Program Files\OCS Inventory Agent\uninst.exe" ("C:\Program Files\OCS Inventory Agent\uninst.exe" /S)
 
-rmdir /s /q "C:\Program FilesOCS Inventory Agent" 2>nul
+rmdir /s /q "C:\Program Files\OCS Inventory Agent" 2>nul
 
 sc delete "OCS Inventory Service" >nul 2>&1
 
